2389850a7de5ffb76ecbbdcd964c89b80a824702
Franco Paredes <francoismaelparedes@hotmail.com>
2026-07-12 13:51:08 -0300

    se empezo a armar el componente Game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,20 @@
+import Timer from "../Timer/Timer";
+import Score from "../Score/Score";
+import WordInput from "../WordInput/WordInput";
+import WordChain from "../WordChain/WordChain";
+import styles from "./Game.module.css";
+
 const Game = () => {
   return (
-    <div>
-        <h1>El juego esta por comenzar</h1>
-        <p>Divertite!</p>
-    </div>
+    <main className={styles.container}>
+      <h1 className={styles.title}>Palabras Encadenadas</h1>
+      <section className={styles.header}>
+        <Timer />
+        <Score />
+      </section>
+      <WordInput />
+      <WordChain />
+    </main>
   );
 }
 
